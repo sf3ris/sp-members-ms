c5dc0051fb49855dc5fdd9c23b41296bb04afac6
Sf3ris <vale.piz92@hotmail.it>
2020-09-08 23:25:38 +0200

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+from routes import routes
+
+app = Flask(__name__)
+app.register_blueprint(routes.routes)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5001)
---
+++ app/routes/routes.py
@@ -0,0 +1,10 @@
+
+import flask 
+from typing import List
+
+routes = flask.Blueprint('routes',__name__)
+
+@routes.route('/', methods=["GET"])
+def home( ) -> flask.Response :  
+
+    return flask.jsonify("Welcome to Python API")
